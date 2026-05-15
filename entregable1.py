f1f1265f179a8b3a113d377f0596d9c9efb6fb7a
JuanPabloSarra <juampablosarra@gmail.com>
2026-05-15 17:34:13 -0300

    def cost

--- entregable1.py
+++ entregable1.py
@@ -9,8 +9,20 @@ class RoverProblem(SearchProblem):
         pass
     def result(self, state, action):
         pass
-    def cost(self, state1, action, state2):
-        pass
+    def cost(self, state, action, state2):
+        if action[0] == "moverse":
+            return 1
+        else if action[0] == "sobremarcha":
+            return 1    
+        else if action[0] == "equipar":
+            return 3
+        else if action[0] == "recolectar":
+            return 2
+        else if action[0] == "depositar":
+            return len(state[5])
+        else if action[0] == "recargar":
+            return 4
+
     def heuristic(self, state):
         pass
     
@@ -21,5 +33,7 @@ def planear_rover (rover_inicio, bateria_inicial, zonas_sombra, muestras_igneas,
         None,                            # taladro equipado
         tuple(muestras_igneas),          # muestras ígneas pendientes
         tuple(muestras_sedimentarias),   # muestras sedimentarias pendientes
-        0,                               # carga actual
+        (),                              # carga actual
     )
+
+    constante_sombras = zonas_sombra
